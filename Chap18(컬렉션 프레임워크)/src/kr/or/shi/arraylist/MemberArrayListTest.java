d45787aa16003ffde1c8a48237421a9be269b8ed
doridorijamjam2 <hoc1018@naver.com>
2021-09-02 11:47:51 +0900

    17day

---
+++ Chap18(컬렉션 프레임워크)/src/kr/or/shi/arraylist/MemberArrayListTest.java
@@ -0,0 +1,27 @@
+package kr.or.shi.arraylist;
+
+public class MemberArrayListTest {
+
+	public static void main(String[] args) {
+		MemberArrayList memberArrayList = new MemberArrayList();
+		
+		Member memberLee = new Member(1001, "이순신");
+		Member memberRyu = new Member(1002, "류현진");
+		Member memberChoi = new Member(1003, "최지만");
+		Member memberKim = new Member(1004, "김하성");
+		
+		memberArrayList.addMember(memberLee);
+		memberArrayList.addMember(memberRyu);
+		memberArrayList.addMember(memberChoi);
+		memberArrayList.addMember(memberKim);
+		
+		memberArrayList.showAllMember();
+
+		memberArrayList.removeMember(memberRyu.getMemberId());
+		memberArrayList.showAllMember();
+		
+		
+		
+	}
+
+}
